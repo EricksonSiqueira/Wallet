93708248d1c456945fd0cf6a1b804915c5fb8d0b
Erickson <ericksonsiqueirasilva@gmail.com>
2021-11-22 11:42:03 -0300

    Requisito 3, colocando email do usuario na tela junto com dinheiro total e moeda atual

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,30 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <header>
+          <h3 data-testid="email-field">{email}</h3>
+          <section>
+            <p data-testid="total-field">0</p>
+            <p data-testid="header-currency-field">BRL</p>
+          </section>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);

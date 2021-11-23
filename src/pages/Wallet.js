a758ff0e3ee3a75b902b0623d2f2197cdbbb27d3
Erickson <ericksonsiqueirasilva@gmail.com>
2021-11-22 23:31:45 -0300

    Requisito 6, criando tabela

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -31,6 +31,21 @@ class Wallet extends React.Component {
           </section>
         </header>
         <WalletForm />
+        <table>
+          <tbody>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
